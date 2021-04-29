91e94498131309d58c47350f7e7953f4b18504ca
Arjun Shah <61455786+arjun-shah-noon@users.noreply.github.com>
2021-04-29 20:23:00 +0530

    login before push

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,19 @@ docker build -t arjunshah/multi-client:latest -t arjunshah/multi-client:$SHA -f 
 docker build -t arjunshah/multi-server:latest -t arjunshah/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t arjunshah/multi-worker:latest -t arjunshah/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-client:latest
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-server:latest
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-worker:latest
 
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-client:$SHA
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-server:$SHA
+echo "$DOCKER_PASSWORD | docker login -u ""$DOCKER_USERNAME" --password-stdin
 docker push arjunshah/multi-worker:$SHA
 
 kubectl apply -f k8s
